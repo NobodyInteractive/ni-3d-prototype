f2a561d6e949ccb9f0c368d8b89b4338ac04ee8b
NobodyInteractive <95953139+NobodyInteractive@users.noreply.github.com>
2022-01-16 01:46:28 +0900

    Start learn wgpu

---
+++ src/learn_wgpu.rs
@@ -0,0 +1,76 @@
+mod the_surface;
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{WindowBuilder, Window}
+};
+
+use async_trait::async_trait;
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>
+}
+
+
+#[async_trait]
+trait Tutorial {
+    async fn new(window: &Window) -> Self;
+    
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>);
+    
+    fn input(&mut self, eevnt: &WindowEvent) -> bool;
+    
+    fn update(&mut self) ;
+    
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError>;
+}
+
+
+pub fn tut_main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = pollster::block_on(State::new(&window));
+
+    event_loop.run(move | event, _, control_flow| match event {
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        },
+        Event::WindowEvent {
+            ref event, window_id
+        } if window_id == window.id() => if !state.input(event) { //update!
+            match event { 
+                
+
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+                WindowEvent::ScaleFactorChanged {new_inner_size, ..} => {
+                    state.resize(**new_inner_size);
+                }
+                _ => {}
+            }
+        },
+        _ =>   {}
+    });    
+}
